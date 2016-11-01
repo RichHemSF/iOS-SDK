1655e7526f8086190ce01ab349bb9a87bb3163ae
Peter Wang <peter.wang@navisens.com>
2016-11-01 15:11:13 -0700

    release 0.1.0

---
+++ CocoapodsExample/MotionDnaSDKCocoaPodsExample/MotionDnaSDKCocoaPodsExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MotionDnaSDKCocoaPodsExample
+//
+//  Created by Peter Wang on 11/1/16.
+//  Copyright © 2016 Navisens Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
